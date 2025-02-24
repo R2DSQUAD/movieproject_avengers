54b39b77328d2dab732ab651d6f07db50d34f067
yyk6591 <yyk6591@gmail.com>
2025-02-24 17:43:25 +0900

    장바구니

--- moviepj/src/main/java/org/spring/moviepj/controller/CartController.java
+++ moviepj/src/main/java/org/spring/moviepj/controller/CartController.java
@@ -1,10 +1,17 @@
 package org.spring.moviepj.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.spring.moviepj.dto.CartItemRequestDto;
+import org.spring.moviepj.entity.CartItemEntity;
+import org.spring.moviepj.repository.CartItemRepository;
 import org.spring.moviepj.service.impl.CartServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +26,9 @@ public class CartController {
 
     private final CartServiceImpl cartServiceImpl;
 
-    @PostMapping("/cart/insert")
+    private final CartItemRepository cartItemRepository;
+
+    @PostMapping("/cart/insert") // 장바구니 추가
     public ResponseEntity<?> addCart(@RequestBody CartItemRequestDto cartItemRequestDto,
             @AuthenticationPrincipal String email) {
 
@@ -29,6 +38,28 @@ public class CartController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
+
+    // @GetMapping("/cart/myCartList")
+    // public ResponseEntity<?> myCartList(@AuthenticationPrincipal String email) {
+
+    // try {
+    // return ResponseEntity.ok(cartServiceImpl.getMyCartList(email));
+    // } catch (IllegalArgumentException e) {
+    // return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    // } catch (Exception e) {
+    // return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("장바구니 정보를
+    // 가져오는 중 오류 발생");
+    // }
+
+    // }
+
+    @GetMapping("/cart/disabledSeats/{screeningId}") // 프론트에서 사용 -> 해당상영스케줄좌석리스트(예약된좌석제외하고)
+    public ResponseEntity<List<String>> getDisabledSeats(@PathVariable Long screeningId) {
+        List<String> disabledSeats = cartItemRepository.findByScreeningEntityId(screeningId).stream()
+                .map(CartItemEntity::getSeatNumber)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(disabledSeats);
     }
 
 }
